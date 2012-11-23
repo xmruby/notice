8615dad07c8423467ddf0e06ad82bae05f587e63
xmruby <menksoft_liang@163.com>
2012-11-23 10:46:01 +0800

    解决群发短信 超时

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,9 +2,12 @@ class Message
   def self.publish_message(ext, city)
     meters = Meter.all
     content = SmsConfig::NOTICE
-
+    i = 0
     meters_valid_phone(meters).each do |meter|
-      SmsConfig::REDIS.publish "SMS","{\"phone\":\"#{meter.User_Phon}\",\"content\":\"#{content}\",\"ext\":\"#{ext}\",\"app\":\"#{SmsConfig::APP}\",\"city\":\"#{city}\"}"
+      # sleep 50
+      # SmsConfig::REDIS.publish "yy","{\"phone\":\"#{meter.User_Phon}\",\"content\":\"#{content}\",\"ext\":\"#{ext}\",\"app\":\"#{SmsConfig::APP}\",\"city\":\"#{city}\"}"
+      SmsConfig::REDIS.publish "yy","{#{i}  ,\"phone\":\"#{meter.User_Phon}\",\"content\":\"#{content}\",\"ext\":\"#{ext}\",\"app\":\"#{SmsConfig::APP}\",\"city\":\"#{city}\"}"
+      i = i + 1
     end
   end
 
--- app/models/sms_config.rb
+++ app/models/sms_config.rb
@@ -3,7 +3,8 @@ class SmsConfig
   CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
   IP = CONFIG["redis_settings"]["ip"]
   PORT = CONFIG["redis_settings"]["port"]
-  REDIS = Redis.new(:host => IP, :port => PORT)
+  TIMEOUT = CONFIG["redis_settings"]["timeout"]
+  REDIS = Redis.new(:host => IP, :port => PORT, :timeout => 180)
   APP = CONFIG["app_settings"]["app"]
   
   NOTICE = CONFIG["notice"]["template"]
